d06514c12b93047156789787f94eab1096b35289
Wahyu Aril Saputra <blackvennom28@gmail.com>
2023-06-16 10:47:21 +0700

    Merge remote changes

--- kelompokpro-master/app.py
+++ kelompokpro-master/app.py
@@ -1,36 +1,27 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 from sequence_split import split_sequence
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.decomposition import PCA
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.neural_network import MLPRegressor
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
-from datetime import datetime, timedelta
-import pickle
+from sklearn.metrics import mean_absolute_percentage_error
+from datetime import datetime
 
 X_train, X_test, y_train, y_test = None, None, None, None
-scaler = MinMaxScaler()
-pca = PCA(n_components=2)
-model = None
-data = pd.read_csv('https://raw.githubusercontent.com/wahyuarilsaputra/dataset/main/BBTN.JK.csv')
-n_steps = 3
-
-
 def main():
     st.sidebar.title("Prediksi Data Time Series Bank BTN")
     menu = ["Data", "Pre processing data", "Implementasi"]
     choice = st.sidebar.selectbox("Menu", menu)
-    global data
-    global n_steps
 
     if choice == "Data":
         st.title("1.Wahyu Aril Saputra 200411100055")
         st.title("2.Billy Morgen Simbolon 200411100057")
         st.markdown("<h4>Data</h4>", unsafe_allow_html=True)
+        data = pd.read_csv('https://raw.githubusercontent.com/wahyuarilsaputra/dataset/main/BBTN.JK.csv')
         
         st.write(data)
         if st.button("Penjelasan"):
@@ -65,7 +56,8 @@ def main():
         st.title("2.Billy Morgen Simbolon 200411100057")
         st.markdown("<h4>Pre processing data</h4>", unsafe_allow_html=True)
         selected_column = st.selectbox("Pilih Kolom", ('Open','High', 'Low', 'Close'))
-        sequence = data[selected_column].astype(float).values
+        data = pd.read_csv('https://raw.githubusercontent.com/wahyuarilsaputra/dataset/main/BBTN.JK.csv')
+        sequence = data[selected_column].astype(int).values
         n_steps = 3
         X, y = split_sequence(sequence, n_steps)
 
@@ -80,10 +72,10 @@ def main():
         col1, col2 = st.columns([1, 2])
         with col1:
             st.markdown("<h6>Data Setelah Dinormalisasi</h6>", unsafe_allow_html=True)
-            # scaler.fit(newFitur)
             scaler = MinMaxScaler()
             X_norm = scaler.fit_transform(newFitur)
             st.write(pd.DataFrame(X_norm, columns=newFitur.columns))
+            # st.empty()  # Menggunakan st.empty() untuk mengatur tata letak di kolom pertama
         
         with col2:
             st.markdown("<h6>Data Setelah Dilakukan PCA</h6>", unsafe_allow_html=True)
@@ -91,39 +83,60 @@ def main():
             X_pca = pca.fit_transform(X_norm)
             st.write(pd.DataFrame(X_pca, columns=['PCA Component 1', 'PCA Component 2']))
         
-        global X_train, X_test, y_train, y_test
         X_train, X_test, y_train, y_test = train_test_split(X_pca, y, test_size=0.2, random_state=0)
         st.markdown("<h3>Modelling</h3>", unsafe_allow_html=True)
-        selected_model = st.selectbox("Pilih Model", ('Decision Tree', 'KNN'))
+        selected_model = st.selectbox("Pilih Model", ('Decision Tree', 'KNN', 'MLP', 'Naive Bayes'))
         if selected_model == "Decision Tree":
-            model = DecisionTreeRegressor()
+            model = DecisionTreeClassifier()
         elif selected_model == "KNN":
-            model = KNeighborsRegressor()
-        model.fit(X_train, y_train)
+            model = KNeighborsClassifier()
+        elif selected_model == "MLP":
+            model = MLPClassifier()
+        elif selected_model == "Naive Bayes":
+            model = GaussianNB()
+        y_pred = model.fit(X_train, y_train)
+        # accuracy = mean_absolute_percentage_error(y_pred, y_test)
         accuracy = model.score(X_test, y_test)
-        y_pred = model.predict(X_test)
         st.write("Akurasi model:", accuracy)
-        # st.write("Model di training.")
 
     elif choice == "Implementasi":
         st.title("1.Wahyu Aril Saputra 200411100055")
         st.title("2.Billy Morgen Simbolon 200411100057")
         st.markdown("Implementasi")
-        input1 = st.number_input('Masukkan data ke-1 : ')
-        input2 = st.number_input('Masukkan data ke-2 : ')
-        input3 = st.number_input('Masukkan data ke-3 : ')
-        input = np.array([input1, input2, input3])
-        input = input.reshape(1, -1)
-        scaler = MinMaxScaler()
-        pca = PCA(n_components=1)
-
-        input = scaler.fit_transform(input)
-        input = pca.fit_transform(input)
+        # Tambahkan kode untuk halaman Implementasi di sini
+        input_date = st.date_input("Date", value=datetime.today())
+        formatted_date = input_date.strftime("%Y-%m-%d")
+        input_row = None
         if st.button("Prediksi"):
-                model_NB = pickle.load(open('model/model_NB.sav', 'rb'))
-                prediksi = model_NB.predict(input)
-                st.write("Prediksi harga saham : ", prediksi)
-            
+             input_row = data[data['Date'] == input_date]
+
+        if input_row.empty:
+            st.write("Data tidak ditemukan")
+        else:
+            # Ambil nilai Open, High, Low, dan Close dari baris data yang ditemukan
+            open_value = input_row['Open'].values[0]
+            high_value = input_row['High'].values[0]
+            low_value = input_row['Low'].values[0]
+            close_value = input_row['Close'].values[0]
+
+            # Bentuk array fitur sesuai dengan format yang diharapkan oleh model
+            input_features = np.array([[open_value, high_value, low_value, close_value]])
+
+            # Normalisasi fitur input
+            input_features_norm = scaler.transform(input_features)
+
+            # Transformasi fitur input menggunakan PCA
+            input_features_pca = pca.transform(input_features_norm)
+
+            # Lakukan prediksi menggunakan model
+            prediction = model.predict(input_features_pca)
+
+            # Tampilkan hasil prediksi
+            st.write("Hasil Prediksi:")
+            st.write("- Open: ", prediction[0, 0])
+            st.write("- High: ", prediction[0, 1])
+            st.write("- Low: ", prediction[0, 2])
+            st.write("- Close: ", prediction[0, 3])
 
 if __name__ == '__main__':
     main()
